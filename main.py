c8caa4962b6efee4bc47fb1f1fd95bba22810e94
DarkhanNW <nwdarkhan@gmail.com>
2025-08-08 22:58:09 +0800

    update stats.py and main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from stats import get_word_count
-
+from stats import get_word_count, character_count
 def main():
     path_to_file = "books/frankenstein.txt"
     print(f"{(get_word_count(path_to_file))} words found in the document")
+    print(character_count(path_to_file))
 
 if __name__ == "__main__":
     main()
+ 
--- stats.py
+++ stats.py
@@ -2,3 +2,16 @@ def get_word_count(path_to_file):
     with open(path_to_file) as f: 
         book_text = f.read()
         return len(book_text.split())
+
+def character_count(path_to_file):
+    character_dict = {}
+    with open(path_to_file) as f:
+        book_text = f.read()
+        lowercase_book_text = book_text.lower()
+        character_list = list(lowercase_book_text)
+        for c in character_list:
+            if c in character_dict:
+                character_dict[c] += 1
+            else:
+                character_dict[c] = 1
+    return character_dict
